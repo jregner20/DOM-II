d7b33fb4e935a5af6330a70f08dfb73343b7d570
Jake Regner <jregner20@gmail.com>
2019-10-01 17:43:42 -0400

    Create 2 selectors for changing color on nav hover and double clicking header

--- js/index.js
+++ js/index.js
@@ -1,1 +1,22 @@
 // Your code goes here
+
+
+// Turns Nav buttons red on hover
+document.querySelectorAll(".nav-link").forEach(el => {
+    el.addEventListener("mouseover", () => {
+        el.style.color="red";
+    })
+});
+
+// Turns Fun Bus header blue on double click
+document.querySelectorAll(".logo-heading");
+funBus.addEventListener("dblclick", () => {
+    funBus.style.color="blue";
+})
+
+
+
+// const headerImg = document.querySelector(".intro img");
+// headerImg.addEventListener("resize", () => {
+//     headerImg.src = "img/The-Bus2.jpg";
+// })
