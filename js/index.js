feb6d606bdcb20a42fe02c88f9dfc138c886f5e6
Jake Regner <jregner20@gmail.com>
2019-10-01 20:54:22 -0400

    Create 2 event listeners that make a content image disappear when clicked on and makes bottom content image bigger when dragged

--- js/index.js
+++ js/index.js
@@ -28,34 +28,44 @@ click.addEventListener("contextmenu", () => {
     click.style.fontSize = "6rem";
 })
 
-//mouseenter style.textShadow
+// Shadows the 1st content header when mouse on
 
 const shadow = document.querySelector(".content-section h2");
 shadow.addEventListener("mouseenter", () => {
     shadow.style.textShadow = "5px 2px 2px";
 })
 
-//mouseleave
+// Underlines the 3rd content header when mouses leaves
 
 const underl = document.querySelector(".content-destination h2");
 underl.addEventListener("mouseleave", () => {
     underl.style.textDecorationLine = "underline";
 })
 
-//keyup
+// When any key is released the background turns purple
 
 const key1 = document.querySelector("body");
 key1.addEventListener("keyup", () => {
     key1.style.background = "purple";
 })
 
-//keydown
+// When any key is press down, the background turns cyan
 
 const key2 = document.querySelector("body");
 key2.addEventListener("keydown", () => {
     key2.style.background = "cyan";
 })
 
-//click
+// When you click on the first content section image, it disappears
 
-//scroll
+const clork = document.querySelector(".content-section img");
+clork.addEventListener("click", () => {
+    clork.style.display = "none";
+})
+
+// When you try to drag the last content image, it gets bigger
+
+const crazy = document.querySelector(".content-destination img");
+crazy.addEventListener("drag", () => {
+    crazy.style.transform = "scale(2)";
+})
